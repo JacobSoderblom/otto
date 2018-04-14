d4534b72006a17e852b2e7fc052724605cbbeb15
Jacob Söderblom <jacob.soderblom@outlook.com>
2018-04-14 10:12:03 +0200

    added funcs to interface

--- context.go
+++ context.go
@@ -20,6 +20,11 @@ type Context interface {
 	Redirect(code int, location string) error
 	Request() *http.Request
 	Response() *Response
+	FormValue(key string) string
+	FormParams() (url.Values, error)
+	QueryParam(key string) string
+	QueryParams() url.Values
+	QueryString() string
 }
 
 type context struct {
@@ -29,15 +34,15 @@ type context struct {
 	query   url.Values
 }
 
-func (c context) Request() *http.Request {
+func (c *context) Request() *http.Request {
 	return c.req
 }
 
-func (c context) Response() *Response {
+func (c *context) Response() *Response {
 	return c.res
 }
 
-func (c context) JSON(code int, val interface{}) error {
+func (c *context) JSON(code int, val interface{}) error {
 	b, err := json.Marshal(val)
 	if err != nil {
 		return errors.Wrap(err, "failed to parse json")
@@ -45,26 +50,26 @@ func (c context) JSON(code int, val interface{}) error {
 	return c.render(code, "application/json", b)
 }
 
-func (c context) HTML(code int, val string) error {
+func (c *context) HTML(code int, val string) error {
 	return c.render(code, "text/html", []byte(val))
 }
 
-func (c context) String(code int, val string) error {
+func (c *context) String(code int, val string) error {
 	return c.render(code, "text/plain", []byte(val))
 }
 
-func (c context) Error(code int, err error) error {
+func (c *context) Error(code int, err error) error {
 	return HTTPError{
 		Code: code,
 		Err:  err,
 	}
 }
 
-func (c context) NoContent() error {
+func (c *context) NoContent() error {
 	return c.render(http.StatusNoContent, "", []byte{})
 }
 
-func (c context) Redirect(code int, location string) error {
+func (c *context) Redirect(code int, location string) error {
 	if (code < 300 || code > 308) && code != 201 {
 		return errors.New("invalid redirect status code")
 	}
@@ -72,25 +77,25 @@ func (c context) Redirect(code int, location string) error {
 	return c.render(code, "", []byte{})
 }
 
-func (c context) FormValue(key string) string {
+func (c *context) FormValue(key string) string {
 	return c.req.FormValue(key)
 }
 
-func (c context) FormParams() (url.Values, error) {
+func (c *context) FormParams() (url.Values, error) {
 	if err := c.parseForm(); err != nil {
 		return nil, errors.Wrap(err, "failed to parse form from request")
 	}
 	return c.req.Form, nil
 }
 
-func (c context) QueryParam(key string) string {
+func (c *context) QueryParam(key string) string {
 	if c.query == nil {
 		c.query = c.req.URL.Query()
 	}
 	return c.query.Get(key)
 }
 
-func (c context) QueryParams() url.Values {
+func (c *context) QueryParams() url.Values {
 	if c.query == nil {
 		c.query = c.req.URL.Query()
 	}
@@ -116,7 +121,7 @@ func (c *context) render(code int, ct string, b []byte) error {
 	return nil
 }
 
-func (c context) parseForm() error {
+func (c *context) parseForm() error {
 	if strings.Contains(c.req.Header.Get(HeaderContentType), MIMEMultipartForm) {
 		return c.req.ParseMultipartForm(30 << 20) // 32MB
 	}
