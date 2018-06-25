9c3607fcec2dbccdc2f22956a9fc5e9457fb2c2c
Jacob Söderblom <jacob.soderblom@outlook.com>
2018-06-25 15:22:09 +0200

    tests

--- app_test.go
+++ app_test.go
@@ -1,8 +1,57 @@
 package otto
 
-import "testing"
+import (
+	"testing"
+	"time"
+
+	"github.com/stretchr/testify/assert"
+)
 
 func Test_APP(t *testing.T) {
 	opts := NewOptions()
-	New(opts)
+	opts.Addr = ":0"
+	app := New(opts)
+
+	go func() {
+		err := app.Serve()
+
+		assert.NoError(t, err)
+	}()
+
+	time.Sleep(200 * time.Millisecond)
+	assert.NoError(t, app.Close(nil))
+}
+
+func Test_UseTLS(t *testing.T) {
+	opts := NewOptions()
+	opts.Addr = ":0"
+	app := New(opts)
+
+	app.UseTLS("testdata/certs/cert.pem", "testdata/certs/key.pem")
+
+	go func() {
+		err := app.Serve()
+
+		assert.NoError(t, err)
+	}()
+
+	time.Sleep(200 * time.Millisecond)
+	assert.NoError(t, app.Close(nil))
+}
+
+func Test_UseAutoTLS(t *testing.T) {
+	opts := NewOptions()
+	opts.Addr = ":0"
+	app := New(opts)
+
+	app.UseAutoTLS("")
+
+	go func() {
+		err := app.Serve()
+
+		assert.NoError(t, err)
+	}()
+
+	time.Sleep(200 * time.Millisecond)
+	assert.NoError(t, app.Close(nil))
 }
